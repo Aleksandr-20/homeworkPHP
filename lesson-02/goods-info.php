6cb1d1cd99a896cbfa6a96091fb04ff8c2a53546
Aleksandr Vysotin <vysotin.dpm@mail.ru>
2022-01-16 15:35:48 +0300

    Домашнее задание к занятию № 2

---
+++ lesson-02/goods-info.php
@@ -0,0 +1,69 @@
+<?php
+
+$items = [
+    [
+        'id' => 1,
+        'title' => 'Flute',
+        'price' => 20000,
+        'img' => 'flute.jpg',
+        'description' => [
+            'color' => 'white',
+            'material' => 'bamboo'
+        ]
+    ],
+    [
+        'id' => 2,
+        'title' => 'Guitar',
+        'price' => 18000,
+        'img' => 'guitar.jpg',
+        'description' => [
+            'color' => 'brown',
+            'material' => 'linden'
+        ]
+    ],
+    [
+        'id' => 3,
+        'title' => 'Drum',
+        'price' => 68000,
+        'img' => 'drum.jpg',
+        'description' => [
+            'color' => 'brown',
+            'material' => 'steel'
+        ]
+    ],
+]; ?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ДЗ к заниятию 2</title>
+</head>
+<body>
+
+    <main>
+		<section class="goods-info">
+        <h2>Все товары</h2>
+
+            <?php foreach ($items as $item): ?>
+                <div class="good-card">
+                    <h3><?= $item['title'] ?></h3>
+                    <p><?= $item['price'] ?></p>
+                    <img height="300" src="/img/<?= $item['img'] ?>" alt="<?= $item['title'] ?>">
+                    <div>
+                        <h4>Описание:</h4>
+                        <span>Цвет: <?= $item['description']['color'] ?></span><br>
+                        <span>Материал: <?= $item['description']['material'] ?></span>
+                    </div>
+                </div>
+            <?php endforeach; ?>   
+
+        </section>		
+	</main>
+    
+</body>
+</html>
+
+
