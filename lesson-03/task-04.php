49db06f0ca9b48104c86a7c4984ce7b1f7b88427
Aleksandr Vysotin <vysotin.dpm@mail.ru>
2022-01-19 15:43:51 +0300

    Занятие 3, задача №4

---
+++ lesson-03/task-04.php
@@ -0,0 +1,66 @@
+<?php
+
+function get_all_tasks(){
+    return [
+        [
+            'title'=>'Задача 1',
+            'date'=>date_create('yesterday'),
+            'participants'=>['Артур', 'Полина'],
+            'closed'=>false
+        ],
+        [
+            'title'=>'Задача 2',
+            'date'=>date_create('tomorrow'),
+            'participants'=>[],
+            'closed'=>false
+        ],
+        [
+            'title'=>'Задача 3',
+            'date'=>date_create(),
+            'participants'=>['Артур', 'Глеб'],
+            'closed'=>false
+        ],
+        [
+            'title'=>'Задача 4',
+            'date'=>date_create('yesterday'),
+            'participants'=>['Павел', 'Полина'],
+            'closed'=>true
+        ]
+    ];
+}
+
+function getNewArray(array $arr) :array
+{
+    $newArr = [];
+    foreach ($arr as $task) {
+        // 1. НОВЫЕ ЗАДАЧИ (Дата задачи > date_create())
+
+        // if ($task['date'] > date_create()) {
+        //     $newArr[] = $task;
+        // }
+        
+        // 2. ЗАКРЫТЫЕ ЗАДАЧИ (со значением closed равным true)
+
+        // if ($task['closed']) {
+        //     $newArr[] = $task;
+        // }
+
+        // 3. НЕВЫПОЛНЕННЫЕ ЗАДАЧИ (со значением closed
+        //  равным false и датой меньше date_create())
+
+        if ($task['closed'] === false && $task['date'] < date_create()) {
+            $newArr[] = $task;
+        }
+    }
+    return $newArr;
+}
+
+$newTasks = getNewArray(get_all_tasks());
+
+function pre($newTasks) {
+    echo '<pre>';
+    var_dump($newTasks);
+    echo "</pre>";
+}
+
+pre($newTasks);
