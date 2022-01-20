8f90478275b54da34b611de24ad079883665a3d1
Aleksandr Vysotin <vysotin.dpm@mail.ru>
2022-01-21 00:27:46 +0300

    Занятие 4, задача №1. Добавлены стили

--- lesson-04/good.php
+++ lesson-04/good.php
@@ -22,8 +22,8 @@ if (!isset($good)) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/goods.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/good.css">
+    <title>Подробнее</title>
 </head>
 <body>
 <section class="good-card">
--- lesson-04/goods.php
+++ lesson-04/goods.php
@@ -10,27 +10,29 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Товары</title>
-    <link rel="stylesheet" href="/css/goods.css">
+    <link rel="stylesheet" href="css/goods.css">
 </head>
 <body>
 <main>
     <h1>Все товары</h1>
+    <section class="goods">
     <? foreach ($goods as $good): ?>
     <div class="good-card">
         <h2><?= $good['title'] ?></h2>
         <div>
             <img src="/images/<?= $good['image'] ?>">
         </div>
-        <p>Стоимость: <?= $good['price'] . ' ₽' ?></p>
+        <p class="price">Стоимость: <?= $good['price'] . ' ₽' ?></p>
 
         <?php if ($good['count'] === 0): ?>
-        <p>Нет в наличии</p>
+        <p class="not-available">Нет в наличии</p>
         <?php else: ?>
-        <a href="good.php?id=<?= $good['id'] ?>">Подробнее</a>
+        <a href="good.php?id=<?= $good['id'] ?>" class="more">Подробнее</a>
         <?php endif; ?>
 
     </div>
     <? endforeach; ?>
+    </section>
 </main>
 </body>
 </html>
