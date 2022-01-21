f78819980e69492d794ff0f41fcd2d4cc1aa656a
Aleksandr Vysotin <vysotin.dpm@mail.ru>
2022-01-21 13:13:02 +0300

    Генерация нового идентификатора 20.01.2022

--- generate-new-id.php
+++ generate-new-id.php
@@ -1,9 +1,12 @@
 <?php
+// реализовать механизм генерации нового id сессии каждые 10 минут
+// для получения временной метки исп-ть ф-ю time()
 
 session_start();
 
-// $_SESSION['time'] = time();
+$_SESSION['time'] = time();
 
-function generateNewID() {
-
+if ($_SESSION['time'] < time() + 600000){
+    session_regenerate_id();   
+    $_SESSION['time'] = time();
 }
