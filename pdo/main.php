84700dee8ef35d79da8bb7feeccb9967b0a870c3
Aleksandr Vysotin <vysotin.dpm@mail.ru>
2022-02-15 16:02:26 +0300

    Главная страница сайта

---
+++ pdo/main.php
@@ -0,0 +1,45 @@
+<?php
+    //Запускаем сессию
+    // session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Название сайта</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+    </head>
+    <body>
+ 
+        <header>
+            <nav id="menu">
+                <a href="main.php">Главная</a>
+                <a href="form-registr.html">Регистрация</a>
+                <a href="form-auth.html">Авторизация</a>
+            </nav>
+        </header>
+
+        <main>
+            <h2>Контент главной страницы</h2>
+            <div id="content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </main>
+
+        <footer>
+            <h2>Подвал сайта</h2>
+        </footer>
+
+    </body>
+</html>
