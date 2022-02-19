fccd59f8609a619caf0dc3489c71502158fcc20b
Aleksandr Vysotin <vysotin.dpm@mail.ru>
2022-02-20 02:28:58 +0300

    Добавлена проверка на наличие данных регистрации в файле

--- lesson-06/task-02/form-handler.php
+++ lesson-06/task-02/form-handler.php
@@ -7,8 +7,47 @@ $post = $_POST;
 $login = $post['login'];
 $password = $post['password'];
 
-if (file_put_contents(USER_REG, $login . ' ' . $password . PHP_EOL, LOCK_EX | FILE_APPEND) !== false){
-    echo 'Данные успешно записаны';
+if (file_exists(USER_REG)) {
+    $arr_from_file = file(USER_REG, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    // перебираем массив, каждый элемент разбиваем по пробелу
+    // получаем логин по индексу 0 и пароль по индексу 1
+    // и сразу сравниваем их
+
+    foreach ($arr_from_file as $value) {
+        $value = explode(" ", $value);
+        
+        if ($value[0] === $login) {
+            echo 'Такой логин уже существует. Придумайте другой';
+            break;
+        } else {
+            if (file_put_contents(USER_REG, $login . ' ' . $password . PHP_EOL, LOCK_EX | FILE_APPEND) !== false){
+                echo 'Данные успешно записаны';
+            }
+            break;
+        }
+    }
+    
+   /* $logins = array();
+
+    foreach ($arr_from_file as $value) {
+        $value = explode(" ", $value);
+        array_push($logins, $value[0]);
+    }
+
+    if (in_array($login, $logins)) {
+        echo 'Такой логин уже существует. Придумайте другой';
+    } else {
+        if (file_put_contents(USER_REG, $login . ' ' . $password . PHP_EOL, LOCK_EX | FILE_APPEND) !== false){
+            echo 'Данные успешно записаны';
+        }
+    } */
+
 } else {
-    echo 'Данные не были записаны';
+    if (file_put_contents(USER_REG, $login . ' ' . $password . PHP_EOL, LOCK_EX | FILE_APPEND) !== false){
+        echo 'Данные успешно записаны';
+    } else {
+        echo 'Данные не были записаны';
+    }
 }
+
+
